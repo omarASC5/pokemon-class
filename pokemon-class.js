b631d1d11669088e20ec7e7ae2cddd14d0a30805
Omar Cobas <41080197+omarASC5@users.noreply.github.com>
2019-03-10 14:09:15 -0400

    Update pokemon-class.js

--- pokemon-class.js
+++ pokemon-class.js
@@ -1,4 +1,5 @@
  class Pokemon {
+	//  The constructor receives user entered arguments, used throughout this class
 	constructor(name, attack, defense, health, type) {
 		this.name = name;
 		this.attack = attack;
@@ -7,12 +8,14 @@
 		this.type = type;
 		this.startingHealth = this.health;
 	}
+	// This method makes the Pokemon lose health, by taking damage, the amount passed in
 	takeDamage(damage) {
 		this.health-=damage;
 		if (this.health < 0) {
 			this.health = 0;
 		}
 	}
+	// This method allows two Pokemon to battle, damaging each other
 	attackOpponent(Pokemon) {
 		let DAMAGE;
 		// If the Pokemon has more defense than the attack --> Take no damage
@@ -21,6 +24,7 @@
 
 		Pokemon.takeDamage(DAMAGE);
 	}
+	// This display() method shows important info about the pokemon
 	display() {
 		return `${this.name.toUpperCase()} (${this.type.toUpperCase()}) ${this.health}/${this.startingHealth}`;
 	}
